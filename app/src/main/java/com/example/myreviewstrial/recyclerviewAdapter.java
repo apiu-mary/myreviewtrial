7d4712a5f679e324949024459ab3ddfb456f5912
apiuuagou@gmail.com <apiuagou@gmail.com>
2023-07-06 08:03:16 +0300

    first commit

---
+++ app/src/main/java/com/example/myreviewstrial/recyclerviewAdapter.java
@@ -0,0 +1,33 @@
+package com.example.myreviewstrial;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class recyclerviewAdapter extends RecyclerView.Adapter<recyclerviewAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public recyclerviewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+
+        public static RecyclerView.ViewHolder extends RecyclerView.ViewHolder{
+            public ViewHolder(View itemView){
+                super(itemView);
+            }
+
+        }
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull recyclerviewAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
